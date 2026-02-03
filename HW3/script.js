9d99b4c3eb98d0b97c142ba31c2b39c2d59415b4
mariagoman <mg138204@umconnect.umt.edu>
2026-02-02 21:07:34 -0700

    Week 3 Homework

---
+++ HW3/script.js
@@ -0,0 +1,22 @@
+// Logging my favorite websites
+console.log("My favorite websites are YouTube, Radiogarden, and Pinterest.");
+
+// Logging my favorite games
+console.log("My favorite games are Animal Crossing, Mario Kart, and Super Smash Bros. Ultimate.");
+
+// Logging favorite artists
+console.log("While there are many artists I love, a few of the most influential to me are Hayao Miyazaki, Mark Crilley, and QinniArts.");
+
+// Explaining cultural importance of each artist
+console.log("Hayao Miyazaki is a highly influential figure in the Japanese animation industry. He helped make Japanese animation well-known and beloved worldwide with his distinct art style.");
+
+console.log("Mark Crilley was an early YouTube art instructor who taught millions of aspiring artists how to create realistic drawings and manga-style art.");
+
+console.log("QinniArts, also known as Qinni Han, was a popular Instagram artist whose beautiful illustrations inspired many artists. Her legacy continues to influence people even after her passing.");
+
+// Simple question-and-answer interaction
+var food = prompt("What is your favorite food?");
+console.log("I like " + food);
+
+
+
